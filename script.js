e3c5b90e268c82eee936ac15fb8e1285425d1a7e
NoeLaurencot <noe.laurencot@edu.univ-fcomte.fr>
2025-10-09 15:08:42 +0200

    Ajout page 3 et réorganisation navbar

--- script.js
+++ script.js
@@ -14,17 +14,14 @@ document.querySelectorAll(".carousel").forEach(carousel => {
 
         buttons.forEach((button, i) => {
             button.addEventListener("click", () => {
-                // Remove active classes from all items and buttons
                 items.forEach(item => item.classList.remove("carousel-item-active"));
                 buttons.forEach(btn => btn.classList.remove("carousel-button-active"));
                 
-                // Add active classes to the clicked button and corresponding item
                 items[i].classList.add("carousel-item-active");
                 button.classList.add("carousel-button-active");
             });
         });
 
-        // Set first button as active by default
         if (buttons.length > 0) {
             buttons[0].classList.add("carousel-button-active");
         }
