81038944cb8244a44a44fd2411cf57e80029bc15
plamour <pierre.lamour@edu.univ-fcomte.fr>
2025-10-13 16:39:23 +0200

    Html/css update + Js for index

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ window.addEventListener('scroll', () => {
     const sectionTop = section.offsetTop;
     const sectionHeight = section.offsetHeight;
 
-    let index = Math.floor((scrollY - sectionTop) / (sectionHeight / 4)); // we divide were we are in the section by 4 so we can get 3image for 3 text (NOT RESPONSIVE)
+    let index = Math.floor(((scrollY - sectionTop)*1.3) / (sectionHeight / 4)); // we divide were we are in the section by 4 so we can get 3image for 3 text (NOT RESPONSIVE)
 
     if (index < 0) index = 0;
     if (index >= images.length) index = images.length - 1;
